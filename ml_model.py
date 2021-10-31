86b49882c0b06aecefd20f816071f160fa7df3c9
jawad26 <msc_jawad@yahoo.com>
2021-10-31 21:00:39 +0530

    Adding 3 New ML Models

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,11 @@
 
 print("Adding all ML related code here")
+
+def model1():
+    print("Adding Code for ML Model1")
+
+def model2():
+    print("Adding Code for ML Model2")
+
+def model3():
+    print("Adding Code for ML Model3")
